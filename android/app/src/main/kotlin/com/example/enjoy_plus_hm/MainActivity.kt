60b513672129721e65a0c95ab31fe50e50f2ced9
yejianhua <yjh_itlike@163.com>
2024-10-08 16:59:25 +0800

    项目初始化

---
+++ android/app/src/main/kotlin/com/example/enjoy_plus_hm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.enjoy_plus_hm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:enjoy_plus_hm/pages/home/index.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
