a0d091ffdc9769f18aca1e7566e6007960131297
yejianhua <yjh_itlike@163.com>
2024-10-08 18:00:07 +0800

    享+社区-配置TabBarPage-下

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
      MaterialApp(
       routes: {
        '/': (context)=> const TabBarPage()
-      }
+      },
      ),
   );
 }
--- lib/pages/tab_bar/index.dart
+++ lib/pages/tab_bar/index.dart
@@ -10,25 +10,60 @@ class TabBarPage extends StatefulWidget {
 }
 
 class _TabBarPageState extends State<TabBarPage> {
+  // 当前选中页面对应的索引
+  int currentIndex = 0;
 
- // 当前选中页面对应的索引
- int currentIndex = 0;  
-  
+  // 底部TabBar数据
+  List tabBarList = [
+    {
+      "title": "首页",
+      "default": "tabs/home_default.png",
+      "active": "tabs/home_active.png"
+    },
+    {
+      "title": "我的",
+      "default": "tabs/my_default.png",
+      "active": "tabs/my_active.png"
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: IndexedStack(
-          // tabBar的索引值
-          index: currentIndex,
-          children: const [
-            HomePage(),
-            MinePage()
-          ],
-        )
-      )
+          child: IndexedStack(
+        // tabBar的索引值
+        index: currentIndex,
+        children: const [HomePage(), MinePage()],
+      )),
+      // 底部导航条
+      bottomNavigationBar: BottomNavigationBar(
+          // 底部导航item的样式
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentIndex,
+          selectedItemColor: const Color.fromARGB(255, 85, 145, 175),
+          unselectedItemColor: Colors.grey,
+          items: barItemList(),
+          onTap: (event) {
+            setState(() {
+              currentIndex = event;
+            });
+          }),
     );
   }
-  
-  
+
+  // 提供一个底部导航条的Item
+  List<BottomNavigationBarItem> barItemList() {
+    List<BottomNavigationBarItem> tempList = [];
+    for (var item in tabBarList) {
+      tempList.add(
+         BottomNavigationBarItem(
+            icon: Image.asset(item['default'], width: 25),
+            activeIcon: Image.asset(item['active'], width: 25),
+            label: item['title']
+          ),
+      );
+    }
+    return tempList;
+  }
 }
