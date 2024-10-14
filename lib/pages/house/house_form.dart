ea9eb748b1c8839a7adf56be50dc9ed5d77757fd
yejianhua <yjh_itlike@163.com>
2024-10-14 17:52:15 +0800

    添加房屋-页面布局-中

--- lib/pages/house/house_form.dart
+++ lib/pages/house/house_form.dart
@@ -8,6 +8,17 @@ class HouseForm extends StatefulWidget {
 }
 
 class _HouseFormState extends State<HouseForm> {
+   final Map _formData = {
+    'point': '', // 小区信息
+    'building': '', // 小区楼栋信息
+    'room': '', // 小区房间信息
+    'name': '', // 业主姓名
+    'gender': 1, // 业主性别0女1男
+    'mobile': '', // 业主电话
+    'idcardFrontUrl': 'assets/images/idcard1.png', // 身份证正面
+    'idcardBackUrl': 'assets/images/idcard2.png', // 身份证背面
+  };
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +30,91 @@ class _HouseFormState extends State<HouseForm> {
           alignment: AlignmentDirectional.bottomEnd,
           children: [
             ListView(
-              children: const [Text('列表')],
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              children: [
+                // 房屋信息
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('房屋信息',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 97, 94, 94),
+                          fontSize: 16)),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 15, bottom: 15),
+                  child: const Text('仙基公寓4单元 1602'),
+                ),
+                // 业主信息
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('业主信息',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 97, 94, 94),
+                          fontSize: 16)),
+                ),
+                // 业主姓名
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: const TextField(
+                    maxLength: 15,
+                    decoration: InputDecoration(
+                      labelText: '姓名',
+                      hintText: '请输入业主姓名',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    )
+                  ),
+                ),
+                 // 性别
+                Container(
+                    color: Colors.white,
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: Row(children: [
+                      const Text(
+                        '性别',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(width: 20),
+                      Radio(
+                        value: 1,
+                        groupValue: _formData['gender'],
+                        onChanged: (value) {
+                          setState(() {
+                            _formData['gender'] = value ?? '';
+                          });
+                        },
+                      ),
+                      const Text('男'),
+                      const SizedBox(width: 10),
+                      Radio(
+                        value: 0,
+                        groupValue: _formData['gender'],
+                        onChanged: (value) {
+                          setState(() {
+                            _formData['gender'] = value ?? '';
+                          });
+                        },
+                      ),
+                      const Text('女'),
+                    ])),
+                  // 业主手机号
+                  Container(
+                    color: Colors.white,
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: const TextField(
+                        keyboardType: TextInputType.phone,
+                        maxLength: 11,
+                        decoration: InputDecoration(
+                          labelText: '手机号',
+                          hintText: '请输入您的手机号',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none,
+                        )),
+                  ),
+              ],
             ),
             Container(
                 padding: const EdgeInsets.all(10),
