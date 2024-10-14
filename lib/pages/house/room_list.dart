12b37a648a6a6572e238f7b8845580359bf85eb5
yejianhua <yjh_itlike@163.com>
2024-10-14 16:54:04 +0800

    选择房间-随机产生房间号

--- lib/pages/house/room_list.dart
+++ lib/pages/house/room_list.dart
@@ -17,16 +17,42 @@ class _RoomListState extends State<RoomList> {
   Map data = {
     'point': '', // 小区信息
     'building': '', // 楼栋信息
-    'rooms': ['102', '203', '1203'], // 随机产生的房间号
+    'rooms': [], // 随机产生的房间号
   };
 
   @override
   void initState() {
     super.initState();
 
+    fakeData();
+  }
+
+  /// 造数据
+  fakeData() {
+    // 1. 随机产生房间的个数
+    Random radom = Random();
+    int size = radom.nextInt(6) + 3; // 3-8
+
+    // 2. 遍历房间的个数产生房间号
+    // 房间号的组成: 楼层0房间  903  1504
+    List temp = [];
+    for (var i = 0; i < size; i++) {
+      // 2.1 随机产生楼层 1-20
+      int floor = radom.nextInt(20) + 1;
+      // 2.2 随机产生房间号 1-4
+      int no = radom.nextInt(4) + 1;
+      // 2.3 把楼层和房间号拼接
+      String room = '${floor}0$no';
+      // 2.5 去重
+      if (temp.contains(room)) continue;
+      // 2.4 把房间号添加到临时数组中
+      temp.add(room);
+    }
+
     setState(() {
       data['point'] = widget.point;
       data['building'] = widget.building;
+      data['rooms'] = temp;
     });
   }
 
